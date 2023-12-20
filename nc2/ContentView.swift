b2150caf2688b3390717e977b3b72959a1dfa88d
lujainalra <l_i_alrasheed@hotmail.com>
2023-12-20 14:23:44 +0300

    Initial Commit

---
+++ nc2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nc2
+//
+//  Created by lujain alrasheed on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nc2/nc2App.swift
@@ -0,0 +1,17 @@
+//
+//  nc2App.swift
+//  nc2
+//
+//  Created by lujain alrasheed on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct nc2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
