9815aa5cdfe91819014f45ad7c00f5ea7657e0a3
NorahAldughem <147074212+NorahAldughem@users.noreply.github.com>
2023-12-24 16:21:49 +0300

    splash screen

--- nc2/ContentView.swift
+++ nc2/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var degreesRotating = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            VStack {
+            
+                Image("black")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea(.all)
+                
+            }
+           
+            
+            Image("Logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 350,height: 330)
+     
+                .padding()
+                .rotationEffect(.degrees(degreesRotating))
+
+                .onAppear {
+                              withAnimation(.linear(duration: 1)
+                                  .speed(0.8).repeatForever(autoreverses: true)) {
+                                      
+                                      degreesRotating = 30
+                                  }
+                    DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+                        withAnimation(.linear(duration: 0.8)
+                            .speed(0.4).repeatForever(autoreverses: true)) {
+                                //degreesRotating = 0
+
+                                degreesRotating = -30
+                            }
+                    }
+           
+                          }
+            
+                
+            Text("شعلة")
+                .font(.system(size:70).bold())
+               
+                .foregroundColor(.white)
+                .padding(.top,450)
+            
+            Text("لنتعلم الحروف مع ")
+                .font(.system(size:70).bold())
+               
+                .foregroundColor(.white)
+                .padding(.bottom,450)
         }
-        .padding()
+        Spacer()
+        
+   
     }
 }
 
